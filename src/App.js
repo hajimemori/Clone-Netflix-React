cb0ad3faa87c50d1b3ee210bf17bb295dd98eb71
Rafael Mori <hajimemori97@gmail.com>
2021-11-12 11:07:00 -0300

    API requests code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect, useState }  from "react";
+import Tmdb from "./Tmdb";
 
 export default () => {
+
+  const [movieList, setMovieList] = useState([])
+
+  useEffect(() => {
+    const loadAll = async() => {
+      // Pegando a lista TOTAL
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+    }
+
+    loadAll()
+  }, [])
+
   return (
-    <div>
-      Olá Mundo!
+    <div className="page">
+      Header
+      Destaque
+      Listas
     </div>
   )
 }
